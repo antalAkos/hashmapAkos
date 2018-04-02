b5f97610d50b0507bb1f8a3a3a126685c7ac2b41
Akos Asztalos <aztalos.akos@gmail.com>
2018-04-02 10:08:32 +0200

    add and get implemented

--- src/main/java/com/codecool/hashmap/MyHashMap.java
+++ src/main/java/com/codecool/hashmap/MyHashMap.java
@@ -1,4 +1,59 @@
 package com.codecool.hashmap;
 
-public class MyHashMap {
+import java.util.LinkedList;
+
+public class MyHashMap<K, V> {
+
+    private int bucketSize = 16;
+
+    // This holds all the data. Its a primitive array where every element is a Linked List.
+// They Linked List holds elements of type KeyValue
+    private LinkedList<KeyValue>[] elements = new LinkedList[bucketSize];
+
+    public void add(String key,  Integer value) {
+        // find out which position of the primitive array to use:
+        int position = getHash(key);
+        for (LinkedList<KeyValue> element : elements) {
+            if (getHash(element.getFirst().getKey()) == position) {
+                throw new IllegalArgumentException("The key already exists!");
+            }
+        }
+        LinkedList list = elements[position];
+        list.add(new KeyValue(key, value));
+
+            // If the key already exists throw an error.
+            // Make a new instance of the KeyValue class, fill it with the key, value parameters, then add it to the list.
+
+            resizeIfNeeded();
+        }
+
+    public Integer getValue(String key) {
+        for (LinkedList<KeyValue> element : elements) {
+            if (getHash(element.getFirst().getKey()) == getHash(key)) {
+                return element.getFirst().getValue();
+            }
+        }
+
+        throw new IllegalArgumentException("There is no such key in the HashMap!");
+
+        // 1. Calculate the hash of the key. This defines which element to get from the "elements" array
+        // 2. Find in the List in this position the KeyValue element that has this key, then return its value.
+        //    If none of the items in the list has this key throw error.
+    }
+
+    private int getHash(String key) {
+        return key.hashCode() % elements.length;
+        // This function converts somehow the key to an integer between 0 and bucketSize
+        // In C# GetHashCode(), in Java hashCode() is a function of Object, so all non-primitive types
+        // can easily be converted to an integer.
+    }
+
+    private void resizeIfNeeded(){}
+    // If it holds more elements than bucketSize * 2, destroy and recreate it
+    // with the double size of the elements array.
+    // if it holds less elements than bucketSize / 2, destroy and recreate it
+    // with half size of the elements array.
+}
+
+
 }
